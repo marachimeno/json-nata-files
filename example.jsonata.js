55240acd65e6c98806c1437c15e49a3032bd0181
Mara Chimeno <50026354+marachimeno@users.noreply.github.com>
2021-07-23 16:33:28 +0100

    Update example.jsonata.js

--- example.jsonata.js
+++ example.jsonata.js
@@ -1,3 +1,16 @@
+/**
+ * JSONata for converting from Cesva-TA120 to NGSI NoiseLevelObserved in its Simplified representation with key-value pairs.
+ *
+ * @author Alexandre Alapetite <https://alexandra.dk/alexandre.alapetite>
+ * @copyright Alexandra Institute <https://alexandra.dk> for the SynchroniCity European project <https://synchronicity-iot.eu> as a contribution to FIWARE <https://www.fiware.org>.
+ * @license MIT
+ * @date 2019-12-17 / 2019-12-20
+ */
 {
-  "name": Info.age
+	"id": NoiseLevelObserved.id,
+	"type": NoiseLevelObserved.type,
+	"LAeq": NoiseLevelObserved.LAeq,
+	"dateObservedFrom": NoiseLevelObserved.dateObserved,
+	"dateObservedTo": NoiseLevelObserved.dateObserved,
+	"location": NoiseLevelObserved.location
 }
